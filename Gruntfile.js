eaf2df20fe046702c7421cbee1f199e238526aa6
Aldream <benjamin.planche@aldream.net>
2015-04-22 21:21:38 +0200

    <admin> Personalizing Bower & Grunt files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,11 @@ module.exports = function(grunt) {
         },
         'gh-pages': {
             options: {
-                clone: 'bower_components/my-repo'
+                clone: 'bower_components/collapse-selector'
             },
             src: [
                 'bower_components/**/*',
-                '!bower_components/my-repo/**/*',
+                '!bower_components/collapse-selector/**/*',
                 'demo/*', 'src/*', 'index.html'
             ]
         },
